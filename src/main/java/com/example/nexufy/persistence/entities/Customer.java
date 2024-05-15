9086245318c28fbfc97e44ab3f9a8901d18847ad
GasparTorres <Gaspartorres352@gmail.com>
2024-05-15 17:59:42 -0300

    Initial commit

---
+++ src/main/java/com/example/nexufy/persistence/entities/Customer.java
@@ -0,0 +1,125 @@
+package com.example.nexufy.persistence.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(unique = true, nullable = false)
+    private int id;
+    @Column
+    private String dni;
+    @Column
+    private String name;
+    @Column
+    private String lastname;
+    @Column
+    private String address;
+    @Column
+    private String email;
+    @Column
+    private String phone;
+    @Column
+    private Date birthdate;
+    @Column
+    private char gender;
+    @Column
+    private Date  registrationdate;
+    @Column
+    private String typeCustomer;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public Date getRegistrationdate() {
+        return registrationdate;
+    }
+
+    public void setRegistrationdate(Date registrationdate) {
+        this.registrationdate = registrationdate;
+    }
+
+    public String getTypeCustomer() {
+        return typeCustomer;
+    }
+
+    public void setTypeCustomer(String typeCustomer) {
+        this.typeCustomer = typeCustomer;
+    }
+}
+
+
