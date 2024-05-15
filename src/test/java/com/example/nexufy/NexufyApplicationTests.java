cfcfb7199c5a4ddbc9c42d7fbfc9dd1c6da5cca4
GasparTorres <Gaspartorres352@gmail.com>
2024-05-15 17:59:42 -0300

    Initial commit

---
+++ src/test/java/com/example/nexufy/NexufyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nexufy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexufyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
