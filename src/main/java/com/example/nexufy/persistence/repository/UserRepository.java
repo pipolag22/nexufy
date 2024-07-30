6108592544f2cfd6e15512e9363c10048ae2c522
GasparTorres <Gaspartorres352@gmail.com>
2024-07-30 18:53:01 -0300

    Controller, Service, Repository Autoincremento de product

--- src/main/java/com/example/nexufy/persistence/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.nexufy.persistence.repository;
-
-import com.example.nexufy.persistence.entities.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-}
