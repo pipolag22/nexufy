c00c0d7a19c463d21db2ee6caef27794b8daff6f
Feezca <fecarrizzo@gmail.com>
2024-09-16 17:20:40 -0300

    AuthController

--- src/main/java/com/example/nexufy/controller/AuthController.java
+++ src/main/java/com/example/nexufy/controller/AuthController.java
@@ -26,7 +26,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
