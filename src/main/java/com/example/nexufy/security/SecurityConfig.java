b3858d784679b14cabf1ba1d84f67b0d99fc4e73
jorge daniel bellerate <112628139+pipolag22@users.noreply.github.com>
2024-10-02 18:12:56 -0300

    Update SecurityConfig.java

--- src/main/java/com/example/nexufy/security/SecurityConfig.java
+++ src/main/java/com/example/nexufy/security/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4280")); // Permitir el origen de tu frontend
+        configuration.setAllowedOrigins(Arrays.asList("https://blue-bay-040a5d710.5.azurestaticapps.net")); // Permitir el origen de tu frontend
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "*")); // Permitir todos los headers
         configuration.setAllowCredentials(true); // Permitir credenciales si es necesario
